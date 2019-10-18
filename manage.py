7e6a679f3cc99d518392a1784c34c93f8d4f7337
bandanarai233 <bandanarai233@gmail.com>
2019-10-18 11:55:28 +0545

    fix bugs

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{ project_name }}.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,4 +18,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main(
+    main()
